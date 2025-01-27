fe41f1f850319e34bca65d54ce3e7634fcaba93e
Glebloom <Gleb88aksenov@gmail.com>
2025-01-28 01:32:51 +0300

    Gleb animation v2

---
+++ levels/back.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="back" tilewidth="16" tileheight="16" tilecount="340" columns="34">
+ <image source="../../../AppData/Local/Temp/Terrain__32x32__RyWhTo.png" width="544" height="160"/>
+</tileset>
---
+++ levels/small cloud 3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="small cloud 3" tilewidth="16" tileheight="16" tilecount="16" columns="8">
+ <image source="../../../AppData/Local/Temp/Small_Cloud_3_QHElys.png" width="140" height="39"/>
+</tileset>
