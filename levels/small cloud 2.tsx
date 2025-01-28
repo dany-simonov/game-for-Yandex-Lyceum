e0ca92f1d59772b7a082c4e74570d99e670d2a98
gleb <gleb88aksenov@gmail.com>
2025-01-28 23:16:10 +0300

    Gleb animation v3

--- levels/small cloud 2.tsx
+++ levels/small cloud 2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="small cloud 2" tilewidth="16" tileheight="16" tilecount="16" columns="8">
- <image source="../../../AppData/Local/Temp/Small_Cloud_2_LvepDu.png" width="133" height="35"/>
+ <image source="Small Cloud 2.png" width="133" height="35"/>
 </tileset>
